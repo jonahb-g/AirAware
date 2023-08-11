cc790e4357e4ee9f402d6fd9e1113ee065524849
Jonah Blaydes-Greenberg <jonahblaydesgreenberg@gmail.com>
2023-08-10 22:37:30 -0700

    Initial Commit

---
+++ AirAware/AirAwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirAwareApp.swift
+//  AirAware
+//
+//  Created by Jonah Blaydes-Greenberg on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AirAwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
